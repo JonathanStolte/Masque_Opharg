a2bfa3871c5f4d440ec16a899047176b8dcb62e4
JonathanStolte <jonathan.stolte@web.de>
2022-02-14 05:52:19 +0100

    Initial commit

---
+++ Masque_Opharg.lua
@@ -0,0 +1,29 @@
+local MSQ, MSQ_Version = LibStub("Masque", true)
+if not MSQ then return end
+
+local Version = GetAddOnMetadata("Masque_Opharg","Version")
+local Websites = {
+	
+}
+
+MSQ:AddSkin("Opharg", {
+	API_VERSION = 90002,
+	Author = "Opharg",
+	Description = "Square border skin with rounded inner edges.",
+	Version = Version,
+	Websites = Websites,
+	Shape = "Square",
+
+
+	Icon = {
+		Width = 38,
+		Height = 38,
+		TexCoords = {0,1,0,1},
+	},
+	Normal = {
+		Width = 38,
+		Height = 38,
+		Color = {0, 0, 0, 1},
+		Texture = [[Interface\AddOns\Masque_Opharg\Textures\Normal_Opharg]],
+	},
+}, true)
---
+++ README.md
@@ -0,0 +1,3 @@
+# Opharg's WeakAura Masque skin
+**Disclaimer:** 
+This only includes a normal texture, which acts as a border skin. Everything else will default to the "Classic" skin, which comes with Masque. It is only really intended for use with WeakAuras
